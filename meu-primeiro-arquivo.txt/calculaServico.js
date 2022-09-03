8484d7329c9f8add829c1eae23891129fb5fc6e4
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-09-02 22:32:30 -0300

    Tentativa 1°

--- meu-primeiro-arquivo.txt/calculaServico.js
+++ meu-primeiro-arquivo.txt/calculaServico.js
@@ -4,3 +4,13 @@ O valor do serviço pode variar conforme o prazo.
 Os valores e prazos devem ser armazenados em uma matriz.
 O cálculo deve ficar obrigatoriamente em uma função.
 */
+let servico = parseInt(prompt('Escolha um dos nossos serviços [1], [2], [3] '));
+
+let horas = parseInt(prompt('Quantidade de horas de espera '));
+
+let valores = ['R$100', 'R$200', 'R$300', 'R$400', 'R$500', 'R$600', 'R$700', 'R$800', 'R$900', 'R$1000'];
+
+document.write(valores.langht);
+
+
+

16661a7da60a18aa8563882bcf5583e6d3978bc3
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-11-05 19:48:36 -0300

    Atualizando branch

--- meu-primeiro-arquivo.txt/ADO III/index.js
+++ meu-primeiro-arquivo.txt/ADO III/index.js
@@ -2,22 +2,18 @@ const urlBase = "http://numbersapi.com";
 
 function request(method, url, data) {
     const promisse = new Promise((resolve, reject) => {
-        // Instancia objeto XMLHttoRequest
         let request = new XMLHttpRequest();
 
-        // Abre a requisição e define o tipo de retorno como json
         request.open(method, url);
         request.responseType = "json";
 
-        // Caso a requisição tenha que enviar um body, define o cabeçalho de content-type
         if(data){
             request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
         }
         
-        // Quando a requisição e carregada, executa uma função 
         request.onload = () => {
 
-            // Caso o stauts seja maior ou igual à 400, acusa como erro, senão, retorna os dados obtidos
+            
             if(request.status >= 400){
                 reject({message: "Erro ao consumir a API."});
             } else{
@@ -25,7 +21,6 @@ function request(method, url, data) {
             }
         }
 
-        // Caso ocorra um rerro na requisição, acusa como erro 
         request.onerror = () => {
             reject({message: "Erro ao consumir a API."})
         }
@@ -37,26 +32,23 @@ function request(method, url, data) {
 }
 
 function burcarCuriosidade() {
-    // Pega os dados digitados pelo usuário
+    
     let numberToPreview = document.getElementById("numberToPreview").value;
 
     if(!numberToPreview || numberToPreview == ""){
         return;
     }
 
-    // Monta a url
     let params = `/${numberToPreview}/math?json`;
     let url = urlBase + params;
 
-    // Realiza a requisição
     let req = request("GET", url);
 
-    // Pega o elemento que será exibido o restorno
     let responseElement = document.getElementById("response");
 
     req
         .then((res) =>{
-            // Caso dê certo, exibe a a curiosidade
+            
             console.log(res);
             if (res){
                 responseElement.innerText = res.text;
@@ -65,12 +57,9 @@ function burcarCuriosidade() {
             }
         })
         .catch((err) =>{
-            // Caso dê erro, exibe a mensagem de erro
             responseElement.innerText = err.message;
         });
 }
-
-// Ao clicar no botão de prever idade, realiza a requisição
 document.getElementById("send").addEventListener("click", function(e){
     e.preventDefault();
 
